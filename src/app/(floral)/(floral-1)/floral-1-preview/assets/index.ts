fe9d9b064ba38a20988bccde0be854be4de49bbf
Josprima Sihombing <jsihombing.id@gmail.com>
2024-07-16 21:35:25 +0700

    add floral2 animation

--- src/app/(floral)/(floral-1)/floral-1-preview/assets/index.ts
+++ src/app/(floral)/(floral-1)/floral-1-preview/assets/index.ts
@@ -2,10 +2,14 @@ import background from "./background.jpg";
 import floral11 from "./floral-1/1.png";
 import floral12 from "./floral-1/2.png";
 import floral13 from "./floral-1/3.png";
+import floral21 from "./floral-2/1.png";
+import floral22 from "./floral-2/2.png";
 
 export const images = [
 	background.src,
 	floral11.src,
 	floral12.src,
 	floral13.src,
+	floral21.src,
+	floral22.src,
 ];
