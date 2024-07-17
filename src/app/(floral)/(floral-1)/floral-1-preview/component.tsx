19fa59ec486b81f775778f2e3350d5ab2bbd2a92
Josprima Sihombing <jsihombing.id@gmail.com>
2024-07-17 15:23:37 +0700

    adjust on mobile

--- src/app/(floral)/(floral-1)/floral-1-preview/component.tsx
+++ src/app/(floral)/(floral-1)/floral-1-preview/component.tsx
@@ -14,6 +14,7 @@ import Floral1 from "./assets/floral-1";
 import Floral2 from "./assets/floral-2";
 import { images } from "./assets";
 import { secondaryFont } from "@/fonts";
+import formatDate from "@/utils/format-date";
 
 export default function Component() {
 	const [loading, setLoading] = useState(true);
@@ -43,13 +44,24 @@ export default function Component() {
 				</div>
 
 				<div className={css.content}>
-					<h1 style={{ fontSize: "18px" }}>WEDDING INVITATION</h1>
+					<h1
+						style={{
+							fontSize: "16px",
+							color: "#fff",
+							letterSpacing: "4px",
+							marginBottom: "12px",
+						}}
+					>
+						WEDDING INVITATION
+					</h1>
 					<h1
 						className={secondaryFont.className}
 						style={{
-							fontSize: "68px",
+							fontSize: "120px",
 							fontWeight: "normal",
-							lineHeight: "52px",
+							lineHeight: "60px",
+							marginBottom: "12px",
+							color: "#fff",
 						}}
 					>
 						{DATA.firstPerson.name}
@@ -58,7 +70,22 @@ export default function Component() {
 						<br />
 						{DATA.secondPerson.name}
 					</h1>
+					<h1
+						style={{
+							fontSize: "16px",
+							color: "#fff",
+							marginBottom: "12px",
+						}}
+					>
+						{formatDate(DATA.isoDate, "DD MMMM YYYY")}
+					</h1>
 					<Countdown date={DATA.isoDate} />
+
+					<div>
+						<h1>Kepada Yth. Bapak / Ibu / Saudara/i</h1>
+						<h2>Tamu Undangan</h2>
+						<button type="button">Buka Undangan</button>
+					</div>
 				</div>
 			</div>
 		</Section>
