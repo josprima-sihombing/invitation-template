b813e3269d97b4aee19a36e302c6c333eb3c9683
Josprima Sihombing <jsihombing.id@gmail.com>
2024-07-17 08:52:49 +0700

    replace keyframes animation with gsap

--- src/app/(floral)/(floral-1)/floral-1-preview/assets/floral-1/index.tsx
+++ src/app/(floral)/(floral-1)/floral-1-preview/assets/floral-1/index.tsx
@@ -1,12 +1,52 @@
+import { useRef } from "react";
 import image1 from "./1.png";
 import image2 from "./2.png";
 import image3 from "./3.png";
 
 import css from "./style.module.css";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 export default function Floral1() {
+	const containerRef = useRef(null);
+
+	useGSAP(
+		() => {
+			const tl = gsap.timeline();
+
+			tl.fromTo(
+				`.${css.floral} img`,
+				{ opacity: 0, scale: 0 },
+				{ opacity: 1, scale: 1, duration: 2 },
+			);
+
+			tl.fromTo(
+				`.${css.floral_1}`,
+				{ rotation: 0 },
+				{ rotation: 90, duration: 5, repeat: -1, ease: "none", yoyo: true },
+			);
+
+			tl.fromTo(
+				`.${css.floral_2}`,
+				{ rotation: -2 },
+				{ rotation: 2, duration: 4, repeat: -1, ease: "sine", yoyo: true },
+				2,
+			);
+
+			tl.fromTo(
+				`.${css.floral_3}`,
+				{ rotation: -2 },
+				{ rotation: 2, duration: 5, repeat: -1, ease: "circ", yoyo: true },
+				2,
+			);
+		},
+		{
+			scope: containerRef,
+		},
+	);
+
 	return (
-		<div className={css.floral}>
+		<div className={css.floral} ref={containerRef}>
 			<img src={image1.src} className={css.floral_1} />
 			<img src={image2.src} className={css.floral_2} />
 			<img src={image3.src} className={css.floral_3} />
