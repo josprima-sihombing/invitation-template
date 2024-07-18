386f37f6ada20a44d60acf0429968f43d2f9f1a1
Josprima Sihombing <jsihombing.id@gmail.com>
2024-07-18 17:10:11 +0700

    add api to handle rsvp form submission

---
+++ src/app/api/[slug]/route.ts
@@ -0,0 +1,46 @@
+import { spreadSheetsId } from "@/spreadsheets-id";
+import { google } from "googleapis";
+
+const auth = new google.auth.GoogleAuth({
+	credentials: {
+		project_id: process.env.google_project_id,
+		private_key_id: process.env.google_private_key_id,
+		private_key: process.env.google_private_key,
+		client_email: process.env.google_client_email,
+		client_id: process.env.google_client_id,
+	},
+	scopes: "https://www.googleapis.com/auth/spreadsheets",
+});
+
+export async function POST(
+	request: Request,
+	{ params }: { params: { slug: string } },
+) {
+	const id = params.slug as keyof typeof spreadSheetsId;
+
+	if (!spreadSheetsId[id]) {
+		return Response.json({ data: "Error!" }, { status: 500 });
+	}
+
+	try {
+		const client = await auth.getClient();
+		const googleSheets = google.sheets({
+			version: "v4",
+			auth: client,
+		});
+
+		await googleSheets.spreadsheets.values.append({
+			auth,
+			spreadsheetId: spreadSheetsId[id],
+			range: "Sheet1!A:E",
+			valueInputOption: "RAW",
+			resource: {
+				values: [["Test", "08126", "Yes", "Nasi Goreng", "Teh Manis"]],
+			},
+		});
+
+		return Response.json({ data: "Hello World" });
+	} catch (error) {
+		return Response.json({ data: "Error!" }, { status: 500 });
+	}
+}
---
+++ src/spreadsheets-id.ts
@@ -0,0 +1,3 @@
+export const spreadSheetsId = {
+	chairi: "1kD0S7ugIe2jL1cVsyouFmV6XEKO91-GYnYiC9T6s5Bk",
+};
