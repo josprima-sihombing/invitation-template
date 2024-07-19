95cddc6a8d71998a74fa82c103e3403537da3619
Josprima Sihombing <jsihombing.id@gmail.com>
2024-07-19 22:04:39 +0700

    set theme color

--- src/app/birthday/(dino)/(dino-1)/layout.tsx
+++ src/app/birthday/(dino)/(dino-1)/layout.tsx
@@ -1,4 +1,9 @@
 import { prodasansFont } from "@/fonts";
+import type { Viewport } from "next";
+
+export const viewport: Viewport = {
+	themeColor: "#3e943e",
+};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return <main className={prodasansFont.className}>{children}</main>;
