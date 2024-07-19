b8624abd486716e191aca21a7ad1693211d68325
Josprima Sihombing <jsihombing.id@gmail.com>
2024-07-20 00:06:08 +0700

    wrap box children in one element root

--- src/app/birthday/(dino)/(dino-1)/chairi/component.tsx
+++ src/app/birthday/(dino)/(dino-1)/chairi/component.tsx
@@ -138,126 +138,136 @@ export default function Component() {
 			</Section>
 			<Section id="section-2">
 				<div className={css.box}>
-					<h1>{guestName ? `Dear ${guestName}` : "Hello"}</h1>
-					<h2>Join us to celebrate</h2>
-					<h3>Chairi 1st Birthday</h3>
-
-					<Countdown date={DATA.isoDate} />
-				</div>
-			</Section>
-
-			<Section>
-				<div className={css.box}>
-					<h1>Save the Date</h1>
-
 					<div>
-						<div>
-							<span>
-								<FaCalendarDay />
-							</span>
-							<span>{formatDate(DATA.isoDate, "dddd, MMMM DD YYYY")}</span>
-						</div>
-						<div>
-							<span>
-								<FaClock />
-							</span>
-							<span>{DATA.time}</span>
-						</div>
-						<div>
-							<span>
-								<FaBuilding />
-							</span>
-							<span>{DATA.place}</span>
-						</div>
-					</div>
+						<h1>{guestName ? `Dear ${guestName}` : "Hello"}</h1>
+						<h2>Join us to celebrate</h2>
+						<h3>Chairi 1st Birthday</h3>
 
-					<div>
-						<button type="button">Detail Location</button>
-					</div>
-
-					<div>
-						<h1>Dress Code</h1>
-						<h2>Pastel Green</h2>
+						<Countdown date={DATA.isoDate} />
 					</div>
 				</div>
 			</Section>
 
 			<Section>
 				<div className={css.box}>
-					<h1>RSVP</h1>
-
-					<form onSubmit={handleSubmit(onSubmit)}>
-						<div>
-							<label htmlFor="name">Your name:</label>
-							<input id="name" type="text" {...register("name")} />
-							<p>{errors.name?.message}</p>
-						</div>
-						<div>
-							<label htmlFor="name">Your Phone Number</label>
-							<input id="name" type="tel" {...register("phoneNumber")} />
-						</div>
+					<div>
+						<h1>Save the Date</h1>
 
 						<div>
-							<input
-								type="radio"
-								id="attend1"
-								value="true"
-								{...register("attend")}
-							/>
-							<label htmlFor="attend1">Will Attend</label>
-							<input
-								type="radio"
-								id="attend2"
-								value="false"
-								{...register("attend")}
-							/>
-							<label htmlFor="attend2">Will Not Attend</label>
-							<p>{errors.attend?.message}</p>
+							<div>
+								<span>
+									<FaCalendarDay />
+								</span>
+								<span>{formatDate(DATA.isoDate, "dddd, MMMM DD YYYY")}</span>
+							</div>
+							<div>
+								<span>
+									<FaClock />
+								</span>
+								<span>{DATA.time}</span>
+							</div>
+							<div>
+								<span>
+									<FaBuilding />
+								</span>
+								<span>{DATA.place}</span>
+							</div>
 						</div>
 
-						{isAttend && (
-							<>
-								<div>
-									<label htmlFor="food">Please choose your desired food</label>
-									<select id="food" {...register("food")} defaultValue="">
-										<option value="" disabled>
-											Choose one option
-										</option>
-										{foods.map((food) => (
-											<option key={food} value={food}>
-												{food}
+						<div>
+							<button type="button">Detail Location</button>
+						</div>
+
+						<div>
+							<h1>Dress Code</h1>
+							<h2>Pastel Green</h2>
+						</div>
+					</div>
+				</div>
+			</Section>
+
+			<Section>
+				<div className={css.box}>
+					<div>
+						<h1>RSVP</h1>
+
+						<form onSubmit={handleSubmit(onSubmit)}>
+							<div>
+								<label htmlFor="name">Your name:</label>
+								<input id="name" type="text" {...register("name")} />
+								<p>{errors.name?.message}</p>
+							</div>
+							<div>
+								<label htmlFor="name">Your Phone Number</label>
+								<input id="name" type="tel" {...register("phoneNumber")} />
+							</div>
+
+							<div>
+								<input
+									type="radio"
+									id="attend1"
+									value="true"
+									{...register("attend")}
+								/>
+								<label htmlFor="attend1">Will Attend</label>
+								<input
+									type="radio"
+									id="attend2"
+									value="false"
+									{...register("attend")}
+								/>
+								<label htmlFor="attend2">Will Not Attend</label>
+								<p>{errors.attend?.message}</p>
+							</div>
+
+							{isAttend && (
+								<>
+									<div>
+										<label htmlFor="food">
+											Please choose your desired food
+										</label>
+										<select id="food" {...register("food")} defaultValue="">
+											<option value="" disabled>
+												Choose one option
 											</option>
-										))}
-									</select>
-								</div>
+											{foods.map((food) => (
+												<option key={food} value={food}>
+													{food}
+												</option>
+											))}
+										</select>
+									</div>
 
-								<div>
-									<label htmlFor="food">Please choose your drink</label>
-									<select id="food" {...register("drink")} defaultValue="">
-										<option value="" disabled>
-											Choose one option
-										</option>
-										{drinks.map((food) => (
-											<option key={food} value={food}>
-												{food}
+									<div>
+										<label htmlFor="food">Please choose your drink</label>
+										<select id="food" {...register("drink")} defaultValue="">
+											<option value="" disabled>
+												Choose one option
 											</option>
-										))}
-									</select>
-								</div>
-							</>
-						)}
+											{drinks.map((food) => (
+												<option key={food} value={food}>
+													{food}
+												</option>
+											))}
+										</select>
+									</div>
+								</>
+							)}
 
-						<div>
-							<button type="submit">Submit</button>
-						</div>
-					</form>
+							<div>
+								<button type="submit">Submit</button>
+							</div>
+						</form>
+					</div>
 				</div>
 			</Section>
 			<Section>
 				<div className={css.box}>
-					<h1>See you there</h1>
-					<h2>Blessed parents</h2>
-					<h3>Radhi & Afira</h3>
+					<div>
+						<h1>See you there</h1>
+						<h2>Blessed parents</h2>
+						<h3>Radhi & Afira</h3>
+					</div>
 				</div>
 			</Section>
 		</div>
