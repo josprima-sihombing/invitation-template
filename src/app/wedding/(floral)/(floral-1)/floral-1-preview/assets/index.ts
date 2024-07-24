abfdc60dba29facc9811c0f76819955eed49d1e4
Josprima Sihombing <jsihombing.id@gmail.com>
2024-07-24 17:12:12 +0700

    template 1 - page1

--- src/app/wedding/(floral)/(floral-1)/floral-1-preview/assets/index.ts
+++ src/app/wedding/(floral)/(floral-1)/floral-1-preview/assets/index.ts
@@ -1,9 +1,13 @@
 import bg from "./bg.jpg";
 import a from "./a.png";
+import b from "./b.png";
+import c from "./c.png";
 
 export const image = {
 	bg: bg.src,
 	a: a.src,
+	b: b.src,
+	c: c.src,
 };
 
 export const images = Object.keys(image).map(
--- src/app/wedding/(floral)/(floral-1)/floral-1-preview/component.tsx
+++ src/app/wedding/(floral)/(floral-1)/floral-1-preview/component.tsx
@@ -10,6 +10,7 @@ import { image, images } from "./assets";
 import DATA from "./data";
 import css from "./style.module.css";
 import { hirarkiSignatureFont, solenoidalFont } from "@/fonts";
+import Countdown from "react-countdown";
 
 export default function Component() {
 	const [loading, setLoading] = useState(true);
@@ -29,29 +30,73 @@ export default function Component() {
 	}
 
 	return (
-		<Section>
-			<div
-				className={css.cover}
-				style={{
-					backgroundImage: `url(${image.bg})`,
-				}}
-			>
-				<h1 className={hirarkiSignatureFont.className}>The Wedding</h1>
+		<>
+			<Section>
+				<div
+					className={css.cover}
+					style={{
+						backgroundImage: `url(${image.bg})`,
+					}}
+				>
+					<h1 className={hirarkiSignatureFont.className}>The Wedding</h1>
 
-				<div className={css.name_container}>
-					<div className={css.name}>
-						<img src={image.a} alt="" />
-						<span className={solenoidalFont.className}>&</span>
-						<h1 className={hirarkiSignatureFont.className}>H</h1>
-						<h2 className={hirarkiSignatureFont.className}>F</h2>
+					<div className={css.name_container}>
+						<div className={css.name}>
+							<img src={image.a} alt="" />
+							<span className={solenoidalFont.className}>&</span>
+							<h1 className={hirarkiSignatureFont.className}>H</h1>
+							<h2 className={hirarkiSignatureFont.className}>F</h2>
+						</div>
 					</div>
-				</div>
 
-				<h2 className={solenoidalFont.className}>January 24th, 2024</h2>
-				<button type="button" className={solenoidalFont.className}>
-					Open Invitation
-				</button>
-			</div>
-		</Section>
+					<h2 className={solenoidalFont.className}>January 24th, 2024</h2>
+					<button type="button" className={solenoidalFont.className}>
+						Open Invitation
+					</button>
+				</div>
+			</Section>
+
+			<Section>
+				<div className={css.page1}>
+					<img src={image.b} alt="" className={css.image1} />
+					<img src={image.c} alt="" className={css.image2} />
+
+					<h1 className={hirarkiSignatureFont.className}>
+						Dear Yudi and Family, <br />
+						You are invited to our wedding
+					</h1>
+
+					<div className={css.name}>
+						<h2 className={solenoidalFont.className}>Hendra Kusuma</h2>
+						<h3 className={solenoidalFont.className}>and</h3>
+						<h2 className={solenoidalFont.className}>Fitri Aja</h2>
+					</div>
+
+					<Countdown
+						date={DATA.isoDate}
+						renderer={({ days, hours, minutes, seconds }) => (
+							<div className={css.countdown}>
+								<div>
+									<span className={solenoidalFont.className}>{days}</span>
+									<span className={solenoidalFont.className}>Days</span>
+								</div>
+								<div>
+									<span className={solenoidalFont.className}>{hours}</span>
+									<span className={solenoidalFont.className}>Hours</span>
+								</div>
+								<div>
+									<span className={solenoidalFont.className}>{minutes}</span>
+									<span className={solenoidalFont.className}>Minutes</span>
+								</div>
+								<div>
+									<span className={solenoidalFont.className}>{seconds}</span>
+									<span className={solenoidalFont.className}>Seconds</span>
+								</div>
+							</div>
+						)}
+					/>
+				</div>
+			</Section>
+		</>
 	);
 }
