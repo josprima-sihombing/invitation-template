50c3f1812ea5182fd3b99a78b3ffefa0f0ee4c5a
Josprima Sihombing <jsihombing.id@gmail.com>
2024-07-27 23:48:54 +0700

    setup template floral 2

---
+++ src/app/wedding/(floral)/(floral-2)/floral-2-preview/component.tsx
@@ -0,0 +1,11 @@
+import Section from "@/components/section";
+
+export default function Component() {
+	return (
+		<>
+			<Section>
+				<h1>Floral 2</h1>
+			</Section>
+		</>
+	);
+}
---
+++ src/app/wedding/(floral)/(floral-2)/floral-2-preview/page.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from "next";
+import Component from "./component";
+import DATA from "./data";
+
+export const metadata: Metadata = {
+	title: DATA.title,
+};
+
+export default function Page() {
+	return <Component />;
+}
