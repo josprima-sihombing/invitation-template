592ec065cbbc1bfae252a45fc4913a88f0a74724
Josprima Sihombing <jsihombing.id@gmail.com>
2024-07-29 19:53:46 +0700

    template 2 - cover

---
+++ src/app/wedding/(floral)/(floral-2)/floral-2-preview/assets/index.ts
@@ -0,0 +1,15 @@
+import bg from "./bg.jpg";
+import a from "./a.svg";
+import b from "./b.png";
+import c from "./c.png";
+
+export const image = {
+	bg: bg.src,
+	a: a.src,
+	b: b.src,
+	c: c.src,
+};
+
+export const images = Object.keys(image).map(
+	(key) => image[key as keyof typeof image],
+);
--- src/app/wedding/(floral)/(floral-2)/floral-2-preview/component.tsx
+++ src/app/wedding/(floral)/(floral-2)/floral-2-preview/component.tsx
@@ -1,10 +1,52 @@
 import Section from "@/components/section";
+import { image } from "./assets";
+
+import css from "./style.module.css";
+import { hirarkiSignatureFont, solenoidalFont } from "@/fonts";
+import { FaEnvelope } from "react-icons/fa6";
 
 export default function Component() {
 	return (
 		<>
 			<Section>
-				<h1>Floral 2</h1>
+				<div
+					className={css.cover}
+					style={{
+						backgroundImage: `url(${image.bg})`,
+					}}
+				>
+					<div className={css.frame}>
+						<div className={css.left} />
+						<div className={css.right} />
+						<div className={css.bottom} />
+						<div className={css.top} />
+
+						<div className={css.frame_name}>
+							<img src={image.a} />
+							<h1 className={solenoidalFont.className}>H&F</h1>
+							<h2 className={solenoidalFont.className}>24.01.26</h2>
+						</div>
+
+						<div className={css.frame_floral}>
+							<img src={image.b} />
+							<img src={image.c} />
+							<img src={image.c} />
+						</div>
+					</div>
+
+					<div className={css.content}>
+						<h1 className={solenoidalFont.className}>THE WEDDING OF</h1>
+						<h2 className={hirarkiSignatureFont.className}>Hendra</h2>
+						<h3 className={hirarkiSignatureFont.className}>and</h3>
+						<h2 className={hirarkiSignatureFont.className}>Fitri</h2>
+						<h4 className={solenoidalFont.className}>Kepada Yth.</h4>
+						<h5 className={solenoidalFont.className}>Tamu Undangan</h5>
+						<button type="button" className={solenoidalFont.className}>
+							<FaEnvelope />
+							<span>Buka Undangan</span>
+						</button>
+					</div>
+				</div>
 			</Section>
 		</>
 	);
