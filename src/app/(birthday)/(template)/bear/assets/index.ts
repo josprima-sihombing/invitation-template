ea21cb9b7e4d542789a1f6d8993bbe46310cdfeb
Josprima Sihombing <jsihombing.id@gmail.com>
2024-09-02 13:39:48 +0700

    add page2

--- src/app/(birthday)/(template)/bear/assets/index.ts
+++ src/app/(birthday)/(template)/bear/assets/index.ts
@@ -3,6 +3,8 @@ import image2 from "./2.png";
 import image3 from "./3.png";
 import image4 from "./4.png";
 import image5 from "./5.png";
+import image6 from "./6.png";
+import image7 from "./7.png";
 
 export const images = {
   image1: image1.src,
@@ -10,4 +12,6 @@ export const images = {
   image3: image3.src,
   image4: image4.src,
   image5: image5.src,
+  image6: image6.src,
+  image7: image7.src,
 };
--- src/app/(birthday)/(template)/bear/page.tsx
+++ src/app/(birthday)/(template)/bear/page.tsx
@@ -8,6 +8,7 @@ import css from "./style.module.scss";
 import { images } from "./assets";
 import Button from "@/components/button";
 import { useEffect, useState } from "react";
+import getName from "@/utils/get-name";
 
 const lemon = Lemon({
   weight: "400",
@@ -16,6 +17,7 @@ const lemon = Lemon({
 
 export default function Page() {
   const [isOpen, setIsOpen] = useState(false);
+  const guestName = getName();
 
   const openInvitation = () => {
     setIsOpen(true);
@@ -28,50 +30,83 @@ export default function Page() {
   }, []);
 
   return (
-    <div className={classNames("h-full relative", css.cover, lemon.className)}>
-      <img
-        src={images.image3}
-        className="w-[80px] absolute right-[30px] bottom-[30px]"
-      />
-      <img
-        src={images.image4}
-        className="w-[110px] absolute left-[30px] bottom-[30px]"
-      />
+    <div
+      className={classNames(lemon.className, "h-full max-w-screen-md mx-auto")}
+    >
+      <div className={classNames("h-full relative z-50", css.cover)}>
+        <img
+          src={images.image3}
+          className="w-[80px] absolute right-[30px] bottom-[30px]"
+        />
+        <img
+          src={images.image4}
+          className="w-[110px] absolute left-[30px] bottom-[30px]"
+        />
 
-      <img
-        src={images.image5}
-        className="w-[250px] absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-[40px]"
-      />
+        <img
+          src={images.image5}
+          className="w-[250px] absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-[40px]"
+        />
 
-      <div className="absolute w-full top-1/2 -translate-y-1/2 text-center">
-        <h1 className="text-center text-white text-lg mb-12">
-          You are invited!
-        </h1>
+        <div className="absolute w-full top-1/2 -translate-y-1/2 text-center">
+          <h1 className="text-center text-white text-lg mb-12">
+            You are invited!
+          </h1>
 
-        <div className="mb-12 relative w-fit mx-auto">
-          <div className="w-[260px] h-[290px] overflow-hidden rounded-[50%] mx-auto relative">
+          <div className="mb-12 relative w-fit mx-auto">
+            <div className="w-[260px] h-[290px] overflow-hidden rounded-[50%] mx-auto relative">
+              <img
+                src={images.image1}
+                className="w-full h-full block object-cover"
+              />
+            </div>
+
             <img
-              src={images.image1}
-              className="w-full h-full block object-cover"
+              src={images.image2}
+              className={classNames(css.center, "w-[320px] max-w-[unset]")}
             />
+
+            <div className="absolute bottom-[3px] left-1/2 -translate-x-1/2">
+              <h1 className="whitespace-nowrap text-white text-lg" id="text">
+                YANTO 1ST BIRTHDAY
+              </h1>
+            </div>
+
+            <div className="absolute w-[100px] h-[380px] bg-[#FBDC77] left-0 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-tr-[80px] -z-10" />
+            <div className="absolute w-[100px] h-[380px] bg-[#E3CFF7] right-0 top-1/2 translate-x-1/2 -translate-y-1/2 rounded-bl-[80px] -z-10" />
           </div>
 
-          <img
-            src={images.image2}
-            className={classNames(css.center, "w-[320px] max-w-[unset]")}
-          />
+          <Button onClick={openInvitation}>OPEN INVITATION</Button>
+        </div>
+      </div>
 
-          <div className="absolute bottom-[3px] left-1/2 -translate-x-1/2">
-            <h1 className="whitespace-nowrap text-white text-lg" id="text">
-              YANTO 1ST BIRTHDAY
-            </h1>
-          </div>
-
-          <div className="absolute w-[100px] h-[380px] bg-[#FBDC77] left-0 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-tr-[80px] -z-10" />
-          <div className="absolute w-[100px] h-[380px] bg-[#E3CFF7] right-0 top-1/2 translate-x-1/2 -translate-y-1/2 rounded-bl-[80px] -z-10" />
+      <div className={classNames(css.page2, "relative pt-32 pb-48 px-8")}>
+        <div className="relative z-10">
+          <h1 className="text-white text-4xl mb-2">
+            Hello{guestName ? ` ${guestName}` : ""}!
+          </h1>
+          <h2 className="text-white text-2xl mb-8">Join us to celebrate</h2>
+          <h3
+            className={classNames(
+              css.title,
+              "text-center text-5xl max-w-[220px]",
+            )}
+          >
+            Yanto <br />
+            1st <br />
+            Birthday
+          </h3>
         </div>
 
-        <Button onClick={openInvitation}>OPEN INVITATION</Button>
+        <div className="h-[300px] w-[50px] bg-[#FBDC77] absolute right-0 top-0 translate-y-[200px] rounded-tr-[100px] rounded-bl-[100px]"></div>
+        <img
+          src={images.image6}
+          className="w-[100px] absolute right-4 top-0 translate-y-[120px]"
+        />
+        <img
+          src={images.image7}
+          className="w-[160px] absolute bottom-[-100px] left-4"
+        />
       </div>
     </div>
   );
