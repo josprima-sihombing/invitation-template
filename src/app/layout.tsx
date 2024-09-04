20666086ad57a4d9734cfe211fde7d4d2a65d1f7
Josprima Sihombing <jsihombing.id@gmail.com>
2024-09-04 18:01:02 +0700

    add google tag

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import Script from "next/script";
+
+export default function RootLayout({
+  children,
+}: { children: React.ReactNode }) {
+  return (
+    <>
+      {children}
+
+      {process.env.NODE_ENV === "production" && (
+        <>
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-6H41XH8NB2"
+            strategy="afterInteractive"
+          />
+          <Script>
+            {`
+              window.dataLayer = window.dataLayer || [];
+
+              function gtag(){dataLayer.push(arguments);}
+
+              gtag('js', new Date());
+              gtag('config', 'G-6H41XH8NB2');
+            `}
+          </Script>
+        </>
+      )}
+    </>
+  );
+}
