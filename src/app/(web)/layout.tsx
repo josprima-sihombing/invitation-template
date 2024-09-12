1851c66e69169db4c7582124544324c8e4b58b3b
Josprima Sihombing <jsihombing.id@gmail.com>
2024-09-12 21:51:27 +0700

    update web meta properties

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -2,6 +2,16 @@ import { Inter } from "next/font/google";
 import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Undangan Digital Website | www.kreasi-invitation.com",
+  description:
+    "Jasa pembuatan undangan pernikahan, tasykuran aqiqah, birthday dan undangan digital lainnya",
+  openGraph: {
+    url: "https://www.kreasi-invitation.com",
+  },
+};
 
 const inter = Inter({ subsets: ["latin"], variable: "--inter-var" });
 
