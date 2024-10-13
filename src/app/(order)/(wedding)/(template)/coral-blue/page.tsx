d0684525f5806c819c437b833a40de4cfb43329f
Josprima Sihombing <jsihombing.id@gmail.com>
2024-10-13 10:52:03 +0700

    add cover content

--- src/app/(order)/(wedding)/(template)/coral-blue/page.tsx
+++ src/app/(order)/(wedding)/(template)/coral-blue/page.tsx
@@ -8,9 +8,33 @@ import css from "./style.module.scss";
 import classNames from "classnames";
 import { sofiaSans, solenoidalFont } from "@/fonts";
 import getName from "@/utils/get-name";
+import dynamic from "next/dynamic";
+import { type AnimationEventHandler, useState } from "react";
+
+import "animate.css";
+
+const Particles = dynamic(() => import("@/components/particles"), {
+  ssr: false,
+});
 
 export default function Page() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [play, setPlay] = useState(false);
+  const [hideMusic, setHideMusic] = useState(true);
+
   const guestName = getName();
+
+  const openInvitation = () => {
+    setIsOpen(true);
+    setPlay(true);
+    setHideMusic(false);
+  };
+
+  const onAnimationEnd: AnimationEventHandler<HTMLDivElement> = (e) => {
+    const target = e.target as Element;
+
+    target.remove();
+  };
 
   return (
     <AssetsLoader assets={{ ...assets, ...photos }}>
@@ -21,9 +45,9 @@ export default function Page() {
         )}
       >
         <div className="h-full relative">
-          <div className="h-full relative bg-[#354279] p-[10px] rounded-[50px]">
+          <div className="h-full relative bg-[#354279] p-[10px] rounded-t-[50px]">
             <div
-              className="relative h-full rounded-[42px] overflow-hidden"
+              className="relative h-full rounded-t-[42px] overflow-hidden"
               style={{
                 backgroundImage: `url(${assets.asset3})`,
                 backgroundSize: "cover",
@@ -32,13 +56,23 @@ export default function Page() {
             >
               <img
                 src={photos.photo1}
-                className="absolute w-full h-full object-cover opacity-30"
+                className={classNames(
+                  {
+                    [css.animate_photo]: isOpen,
+                  },
+                  "absolute w-full h-full object-cover opacity-30",
+                )}
               />
               <div
                 className={classNames(
+                  "animate__animated",
+                  {
+                    animate__fadeOutUp: isOpen,
+                  },
                   css.gradient1,
-                  "relative px-8 py-8 flex items-center justify-between",
+                  "absolute w-full top-0 px-8 py-8 flex items-center justify-between",
                 )}
+                onAnimationEnd={onAnimationEnd}
               >
                 <div
                   className={classNames(
@@ -63,15 +97,54 @@ export default function Page() {
                   <span className="text-[48px]">25</span>
                 </div>
               </div>
+
+              {isOpen && (
+                <div className="absolute w-full top-1/2 -translate-y-1/2">
+                  <div
+                    className={classNames(
+                      "animate__animated animate__delay-1s",
+                      {
+                        animate__fadeInUp: isOpen,
+                      },
+                    )}
+                  >
+                    <p
+                      className={classNames(
+                        solenoidalFont.className,
+                        "text-xl text-center leading-relaxed px-4 text-[#22294A]",
+                      )}
+                    >
+                      Sebab pada awal dunia, Allah menjadikan mereka laki-laki
+                      dan perempuan, sebab itu laki-laki akan meninggalkan
+                      ayahnya dan ibunya dan bersatu dengan istrinya, sehingga
+                      keduanya itu menjadi satu daging. <br />
+                      Demikianlah mereka bukan lagi dua, melainkan satu. Karena
+                      itu, apa yang telah dipersatukan Allah, tidak boleh
+                      diceraikan manusia.
+                      <br />
+                      <br />
+                      Markus 10:6-9
+                    </p>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
+
           <div
-            className="absolute bottom-0 w-full h-1/2 rounded-tr-[calc(100%-80px)]"
+            className={classNames(
+              "animate__animated",
+              {
+                animate__fadeOutDown: isOpen,
+              },
+              "absolute bottom-0 w-full h-1/2 rounded-tr-[calc(100%-80px)]",
+            )}
             style={{
               backgroundImage: `url(${assets.asset1})`,
               backgroundSize: "cover",
               backgroundPosition: "center",
             }}
+            onAnimationEnd={onAnimationEnd}
           >
             <div className="absolute bottom-0 w-full h-[240px]">
               <div className="absolute w-full bottom-0">
@@ -93,6 +166,7 @@ export default function Page() {
                     css.button,
                     "absolute right-[50px] bottom-[40px] text-center leading-none font-bold text-[#22294A]",
                   )}
+                  onClick={openInvitation}
                 >
                   Buka <br />
                   Undangan
@@ -114,7 +188,14 @@ export default function Page() {
             </div>
           </div>
         </div>
+
+        {isOpen && (
+          <div>
+            <h1>Content</h1>
+          </div>
+        )}
       </main>
+      <Particles />
     </AssetsLoader>
   );
 }
