46c5393cc59fcea8f845540417837bf372be80d6
Josprima Sihombing <jsihombing.id@gmail.com>
2024-12-11 07:28:13 +0700

    anggi-ican

--- src/components/new-music/index.tsx
+++ src/components/new-music/index.tsx
@@ -27,79 +27,80 @@ export default function Music({
   setPlay,
   hide = true,
 }: MusicProps) {
-  const [player, setPlayer] = useState(null);
+  return null;
+  // const [player, setPlayer] = useState(null);
 
-  const playMusic = useCallback(() => {
-    setPlay(true);
-  }, []);
+  // const playMusic = useCallback(() => {
+  //   setPlay(true);
+  // }, []);
 
-  const stopMusic = useCallback(() => {
-    setPlay(false);
-  }, []);
+  // const stopMusic = useCallback(() => {
+  //   setPlay(false);
+  // }, []);
 
-  useEffect(() => {
-    if (!player) {
-      return;
-    }
+  // useEffect(() => {
+  //   if (!player) {
+  //     return;
+  //   }
 
-    console.log(player);
-    console.log(player.getVideoData());
+  //   console.log(player);
+  //   console.log(player.getVideoData());
 
-    if (play) {
-      player.playVideo();
-    } else {
-      player.pauseVideo();
-    }
-  }, [play, player]);
+  //   if (play) {
+  //     player.playVideo();
+  //   } else {
+  //     player.pauseVideo();
+  //   }
+  // }, [play, player]);
 
-  const onPlayerReady = (event) => {
-    setPlayer(event.target);
-  };
+  // const onPlayerReady = (event) => {
+  //   setPlayer(event.target);
+  // };
 
-  useEffect(() => {
-    window.addEventListener("blur", stopMusic);
-    window.addEventListener("focus", playMusic);
+  // useEffect(() => {
+  //   window.addEventListener("blur", stopMusic);
+  //   window.addEventListener("focus", playMusic);
 
-    window.onYouTubeIframeAPIReady = () => {
-      new window.YT.Player("player", {
-        height: "390",
-        width: "640",
-        videoId: "3G8CM-6BZC4",
-        videoQuality: "small",
-        playerVars: {
-          playsinline: 1,
-          controls: 0,
-          loop: 1,
-          rel: 0,
-          autoplay: 0,
-        },
-        events: {
-          onReady: onPlayerReady,
-        },
-      });
-    };
+  //   window.onYouTubeIframeAPIReady = () => {
+  //     new window.YT.Player("player", {
+  //       height: "390",
+  //       width: "640",
+  //       videoId: "3G8CM-6BZC4",
+  //       videoQuality: "small",
+  //       playerVars: {
+  //         playsinline: 1,
+  //         controls: 0,
+  //         loop: 1,
+  //         rel: 0,
+  //         autoplay: 0,
+  //       },
+  //       events: {
+  //         onReady: onPlayerReady,
+  //       },
+  //     });
+  //   };
 
-    return () => {
-      window.removeEventListener("blur", stopMusic);
-      window.removeEventListener("focus", playMusic);
-    };
-  }, []);
+  //   return () => {
+  //     window.removeEventListener("blur", stopMusic);
+  //     window.removeEventListener("focus", playMusic);
+  //   };
+  // }, []);
 
-  return (
-    <>
-      <Script src="https://www.youtube.com/iframe_api" strategy="lazyOnload" />
+  // return (
+  //   <>
+  //     <Script src="https://www.youtube.com/iframe_api" strategy="lazyOnload" />
 
-      {play ? (
-        <button className={css.music} type="button" onClick={stopMusic}>
-          <FaPause className={css.music_icon} />
-        </button>
-      ) : (
-        <button className={css.music} type="button" onClick={playMusic}>
-          <FaPlay className={css.music_icon} />
-        </button>
-      )}
+  //     {play ? (
+  //       <button className={css.music} type="button" onClick={stopMusic}>
+  //         <FaPause className={css.music_icon} />
+  //       </button>
+  //     ) : (
+  //       <button className={css.music} type="button" onClick={playMusic}>
+  //         <FaPlay className={css.music_icon} />
+  //       </button>
+  //     )}
 
-      <div id="player" className="hidden"></div>
-    </>
-  );
+  //     <div id="player" className="hidden"></div>
+  //   </>
+  // );
 }
